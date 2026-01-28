a71500ce455b2243a579bdc115683c6e44b32665
vijaypatel12 <vijaygurjarppi>
2026-01-28 15:21:45 +0530

    DSA

--- lecture2_21-01-26.cpp/pattern_ques_03.cpp
+++ lecture2_21-01-26.cpp/pattern_ques_03.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n=6;
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
             cout<<"* ";
         }
         cout<<endl;
