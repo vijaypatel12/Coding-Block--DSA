955ac5a74c179ee6ca6623f765ad95cb18e1abec
vijaypatel12 <vijaygurjarppi>
2026-02-02 11:41:14 +0530

    dsa

--- lecture5_2-02-26/decimal-binary.cpp
+++ lecture5_2-02-26/decimal-binary.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int bin,decimal=0,base=2;
-    cin>>bin;
-
-    
+    int bin=0,decimal,place=1;
+    cin>>decimal;
+    while(decimal>0){
+    int rem=decimal%2;
+    bin=bin+rem*place;
+    place=place*10;
+    decimal=decimal/2;
 }
+cout<<bin;
+
+
+}
+
